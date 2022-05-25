a84d1db944831aae068e4eccb7bd047090fb4d51
Looma1116 <jksjsmlove2@naver.com>
2022-05-25 19:02:12 +0900

    [ADD] project setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    apiKey: "AIzaSyCU0DvFIUOAcHa-2J0AMg3CYblmn7pj3RM",
+    authDomain: "peoplevoice-fcea9.firebaseapp.com",
+    projectId: "peoplevoice-fcea9",
+    storageBucket: "peoplevoice-fcea9.appspot.com",
+    messagingSenderId: "1081065997313",
+    appId: "1:1081065997313:web:e542c43f8f09f1c4166c00",
+    measurementId: "G-PXYT3B4M4Z",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
