30ea72f32e750750de22d563ed3b74110f1c3a87
Looma1116 <jksjsmlove2@naver.com>
2022-06-01 20:44:11 +0900

    [FIX] Nav

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import styles from "./styles.module.css";
 import { useRouter } from "next/router";
