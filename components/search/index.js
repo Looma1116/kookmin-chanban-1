d0f84c2e26522cc91c6fbd6ec8fd69b001d1eb5f
dongsuseo <poopoo0515@naver.com>
2022-06-05 18:56:00 +0900

    [ADD]검색 기능 추가

--- components/search/index.js
+++ components/search/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { searchState, submitState } from "../recoil/recoil";
 import { useRecoilState } from "recoil";
 
 const search = () => {
   const [SearchState, setSearchState] = useRecoilState(searchState);
   const [submit, setSubmit] = useRecoilState(submitState);
+  useEffect(()=>{
+
+  })
   const submitHandler = (event) => {
     event.preventDefault();
     setSubmit((prev) => !prev);
@@ -13,7 +16,7 @@ const search = () => {
     setSearchState(e.target.value);
   };
   const onKeyPress = (e) => {
-    if (e.key == "Enter") {
+    if (e.key === "Enter") {
       submitHandler(e);
     }
   };
@@ -25,7 +28,7 @@ const search = () => {
           placeholder="시민찬반 제목 검색"
           value={SearchState}
           onChange={onChangeHandler}
-          onKeyDown={onKeyPress}
+          onKeyUp={onKeyPress}
         />
       </form>
     </div>
