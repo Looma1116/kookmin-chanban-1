8ba7ff431860f12184573b08ea49282c91860e39
Looma1116 <jksjsmlove2@naver.com>
2022-07-04 15:55:39 +0900

    [ADD] firebase연결중

--- components/modal/editAgenda/index.js
+++ components/modal/editAgenda/index.js
@@ -1,22 +1,64 @@
 import React, { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
 import Modal from "react-modal";
 
+import { useRecoilState } from "recoil";
+import { loginState } from "../../recoil/recoil";
+
+import { getAuth, signInWithCustomToken, updateProfile } from "firebase/auth";
+import { getFirestore, setDoc, doc, getDoc } from "firebase/firestore";
+
 const EditAgenda = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+  const [warningModalIsOpen, setWarningModalIsOpen] = useState(false);
+  const isLoggedIn = useRecoilState(loginState);
+  const auth = getAuth();
+  const db = getFirestore();
+
+  const onSubmitHandler = async (event) => {
+    // USER에 wroteAgenda 추가
+    // userAgenda에 추가
+    uid = auth.currentUser.uid;
+    title = event.target.title.value;
+    subTitle = event.taget.subTitle.value;
+    article = event.target.article.value;
+  };
+
   return (
     <>
-      <button onClick={() => setModalIsOpen(true)}>새 글</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        <button onClick={() => setModalIsOpen(false)}>뒤로</button>
-        <form>
+      <button
+        onClick={
+          isLoggedIn
+            ? () => setEditModalIsOpen(true)
+            : setWarningModalIsOpen(true)
+        }
+      >
+        새 글
+      </button>
+      <Modal
+        isOpen={editModalIsOpen}
+        onRequestClose={() => setEditModalIsOpen(false)}
+      >
+        <button onClick={() => setEditModalIsOpen(false)}>뒤로</button>
+        <form onSubmit={onSubmitHandler}>
           <div>제목</div>
-          <input type="text"></input>
+          <input type="text" name="title"></input>
+          <div>부제목</div>
+          <input type="text" name="subTitle"></input>
           <div>본문</div>
-          <textarea></textarea>
+          <textarea name="article"></textarea>
           <br></br>
-          <input type="file"></input>
+          <input type="file" name="image"></input>
           <button type="submit">등록</button>
+        </form>
+      </Modal>
+      <Modal
+        isOpen={warningModalIsOpen}
+        onRequestClose={() => setWarningModalIsOpen(false)}
+      >
+        <button onClick={() => setEditModalIsOpen(false)}>닫기</button>
+        <form onSubmit={onSubmitHandler}>
+          <div>3초만에 시민찬반에 참여해보세요!</div>
+          <div>로그인</div>
         </form>
       </Modal>
     </>
