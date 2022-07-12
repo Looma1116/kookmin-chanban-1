a8f7590da06d8017595851a48379fe75562a0c51
SOOYOUNG <itsurboiace00@naver.com>
2022-07-12 16:23:57 +0900

    [ADD] 로그인 모달 css

--- components/modal/login/index.js
+++ components/modal/login/index.js
@@ -3,24 +3,31 @@ import LogIn from "../../KAKAO/login";
 import { useState } from "react";
 import { clickCountState } from "../../recoil/recoil";
 import { useRecoilState } from "recoil";
+import styles from "./loginModal.module.css";
 
 const Login = () => {
-   const [modalIsOpen, setModalIsOpen] = useState(true);
-   const [clickCount, setClickCount] = useRecoilState(clickCountState);
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const [clickCount, setClickCount] = useRecoilState(clickCountState);
 
-  const outHandler = ()=>{
+  const outHandler = () => {
     setModalIsOpen(false);
     setClickCount(false);
-  }
+  };
 
-   return (
-     <>
-       <Modal isOpen={modalIsOpen} onRequestClose={outHandler}>
-         <LogIn/>
-         <button onClick={outHandler}>닫기</button>
-       </Modal>
-     </>
-   );
+  return (
+    <>
+      <Modal
+        className={styles.login}
+        isOpen={modalIsOpen}
+        onRequestClose={outHandler}
+      >
+        <LogIn />
+        <button className={styles.close} onClick={outHandler}>
+          닫기
+        </button>
+      </Modal>
+    </>
+  );
 };
 
 export default Login;
