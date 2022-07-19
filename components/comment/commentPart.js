e285bfecb5fa7bd477d226492926fd749937e639
dongsuseo <poopoo0515@naver.com>
2022-07-19 19:15:40 +0900

    [FIX]무한렌더링오류

--- components/comment/commentPart.js
+++ components/comment/commentPart.js
@@ -20,11 +20,12 @@ import {
   getDoc,
   setDoc,
   arrayUnion,
+  addDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { getAuth } from "firebase/auth";
 import styles from "../bestComment/Bestcomments.module.css";
-import LikePart from "./likePart";
 
 const CommentPart = ({ isSubmit }) => {
   const router = useRouter();
@@ -43,10 +44,10 @@ const CommentPart = ({ isSubmit }) => {
   useEffect(() => {
     console.log(isSubmit);
     setCommentData([]);
-    commentFetch();
+    commentFetch(); //댓글 데이터 가져오기
     console.log(commentS);
     console.log(commentData);
-  }, [commentS, isSubmit]);
+  }, [commentS, isSubmit, likeClick]);
 
   const commentFetch = async () => {
     console.log(commentS);
@@ -75,6 +76,61 @@ const CommentPart = ({ isSubmit }) => {
     }
   }
 
+  const beforeLiked = async (commentData) => {
+    //이전에 좋아요 눌렀는지 확인하기
+    let q = query(
+      collection(db, "user", `${auth.currentUser.uid}`, "likeComment"),
+      where("commentId", "==", `${commentData.id}`)
+    );
+    let snapShot = await getDocs(q);
+    if (snapShot.docs.length == 0) {
+      setLikeClick(false);
+      console.log("좋아요한 댓글이 없음");
+    } else {
+      console.log("좋아요 누른 댓글 있음");
+      setLikeClick(true);
+    }
+  };
+  function LikePart({commentData}) {
+    beforeLiked(commentData);
+    if (likeClick) {
+      return <div className={styles.likeBtn} onClick={deleteHandler(commentData)}>{commentData.like}</div>;
+    } // 좋아요 눌러져있는 css
+    else {
+      return <div onclick={addHandler(commentData)}>{commentData.like}</div>;
+    } //좋아요 안눌러진 css
+  }
+
+  const addHandler = async(data) => {
+    console.log("추가 클릭!");
+    await addDoc( //유저 정보에 좋아요 한 댓글 추가
+      collection(db, "user", `${auth.currentUser.uid}`, "likeComment"),
+      {
+        article: data.article,
+        commentAuthorId: data.author,
+        commentId: data.id,
+      }
+    );
+    // 좋아요 한 댓글에 좋아요 수 업데이트
+    await updateDoc(doc(db, `${community}`, `${router.query.id}`, `${commentS}`, `${data.id}`),{
+      like : data.like+1,
+    })
+    setLikeClick(true);
+  };
+
+  const deleteHandler = async(data)=>{
+    console.log("취소 클릭");
+    let q = query(collection(db, "user", `${auth.currentUser.uid}`, "likeComment"),where("commentId","==", `${data.id}`))
+    let deleteSnapShot = await getDocs(q);
+    deleteSnapShot.docs?.forEach((deletedoc)=>{
+      deleteDoc(deletedoc);
+    })
+    await updateDoc(doc(db, `${community}`, `${router.query.id}`, `${commentS}`, `${data.id}`),{
+      like : data.like-1,
+    })
+    setLikeClick(false);
+  }
+
   return (
     <div className={styles.commentlist}>
       {commentData != "" ? (
@@ -88,7 +144,7 @@ const CommentPart = ({ isSubmit }) => {
                 <header className={styles.header}>
                   <Author level={data.authorLevel} />
                   <div className={styles.name}>&nbsp;{data.authorName}</div>
-                  <LikePart commentData={data}/>
+                  {logIn?<LikePart commentData={data} />:<span>{data.like}</span>}
                 </header>
                 <div className={styles.textArea}>{data.article}</div>
               </div>
