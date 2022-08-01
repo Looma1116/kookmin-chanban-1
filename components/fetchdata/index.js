8fd4f6d325bb28ea8d0e8d3860e05d6f230cf512
Dongsu Seo <poopoo0515@naver.com>
2022-08-02 00:21:26 +0900

    [FIX]시민찬반 페이지 staticProps 제대로 적용되게 구조 변경

--- components/fetchdata/index.js
+++ components/fetchdata/index.js
@@ -30,64 +30,52 @@ const FetchData = ({ fetchedData }) => {
   let data = [];
 
   useEffect(() => {
+    console.log(fetchedData);
     dataFetch();
   }, [category, sort, submit]);
 
   const dataFetch = async () => {
-    const wroteAgendaRef = "";
-    if (category == "전체") {
-      wroteAgendaRef = query(collection(db, "userAgenda"));
-    } else {
-      wroteAgendaRef = query(
-        collection(db, "userAgenda"),
-        where("category", "==", category)
-      );
-    }
-
-    const testSnapshot = await getDocs(wroteAgendaRef);
-
-    testSnapshot.forEach(async(document) => {
-      if(document.data().imageUrl==""){
-        if (document.data().category == "정치") {
-          await updateDoc(doc(db, "userAgenda", `${document.id}`), {
-            imageUrl:
-              "https://firebasestorage.googleapis.com/v0/b/peoplevoice-fcea9.appspot.com/o/94043_307275_2538.jpg?alt=media&token=c4f4dd7f-53d0-44b0-a5b1-09f456198867",
-          });
-          console.log("배경화면 업데이트");
-        } else if (document.data().category == "연애") {
-          await updateDoc(doc(db, "userAgenda", `${document.id}`), {
-            imageUrl:
-              "https://firebasestorage.googleapis.com/v0/b/peoplevoice-fcea9.appspot.com/o/pngtree-love-letter-icon-design-template-vector-isolated-png-image_856595.jpg?alt=media&token=12de30e6-3790-47df-a7c4-3a15213b50ff",
-          });
-          console.log("배경화면 업데이트");
-        } else if (document.data().category == "진로") {
-          await updateDoc(doc(db, "userAgenda", `${document.id}`), {
-            imageUrl:
-              "https://firebasestorage.googleapis.com/v0/b/peoplevoice-fcea9.appspot.com/o/%EC%A7%84%EB%A1%9C%EC%B2%B4%ED%97%98%EC%BD%94%EB%94%94%EB%84%A4%EC%9D%B4%ED%84%B01-768x438.png?alt=media&token=25c3f6f2-97e0-4233-af3d-fd96b70df6a9",
-          });
-          console.log("배경화면 업데이트");
-        }
-      }
-      const timeStamp = new Date();
-      //console.log(timeStamp.getTime()); //현재 시간을 초단위로 출력 => 나노초 단위로 출력된다.
-      //console.log(document.data().created.seconds); //게시물이 만들어진 시간을 초단위로 출력
-      const diffDate = timeStamp.getTime() - document.data().created.seconds * 1000;
+    const timeStamp = new Date();
+    //console.log(timeStamp.getTime()); //현재 시간을 초단위로 출력 => 나노초 단위로 출력된다.
+    //console.log(document.data().created.seconds); //게시물이 만들어진 시간을 초단위로 출력
+    for (let i= 0; i < fetchedData.length; i++) {
+      const diffDate =
+        timeStamp.getTime() - fetchedData[i].created.seconds * 1000;
       const dateDays = parseInt(Math.abs(diffDate / (1000 * 3600 * 24))); // 차이 일수 계산
-      if (dateDays <= sort) {
-        if (search === null || search === "") {
-          data.push({ ...document.data(), id: document.id });
-        } else {
-          if (
-            document
-              .data()
-              .title.replace(/ /gi, "")
-              .includes(search.replace(/ /gi, ""))
-          ) {
-            data.push({ ...document.data(), id: doc.id });
+      if ((category = "전체")) {
+        if (dateDays <= sort) {
+          console.log(dateDays);
+          console.log(sort);
+          if (search === null || search === "") {
+            data.push({ ...fetchedData[i], id: fetchedData[i].id });
+          } else {
+            if (
+              fetchedData[i]
+                .title.replace(/ /gi, "")
+                .includes(search.replace(/ /gi, ""))
+            ) {
+              data.push({ ...document.data(), id: doc.id });
+            }
+          }
+        }
+      } else if (fetchedData[i].category == category) {
+        if (dateDays <= sort) {
+          if (search === null || search === "") {
+            data.push({ ...fetchedData[i], id: fetchedData[i].id });
+          } else {
+            if (
+              fetchedData[i].title
+                .replace(/ /gi, "")
+                .includes(search.replace(/ /gi, ""))
+            ) {
+              data.push({ ...document.data(), id: doc.id });
+            }
           }
         }
       }
-    });
+    }
+    console.log(data);
+
     setAgenda(data);
   };
 
